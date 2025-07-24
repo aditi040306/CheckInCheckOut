48274667e4437948d9d870a1ee32a8eb4580edc3
aditiverma <aditi0603verma@gmail.com>
2025-07-23 19:03:30 -0500

    add comments for author

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,6 +1,9 @@
 from flask import jsonify, session
 from ..models.user import find_user_by_username, create_user
 
+# Author : av42956 
+# Controller : to manage login and register user
+
 def handle_register(request):
     data = request.get_json()
     username = data.get('username')
