c39a87e570ed6175940699fc0b917152fa93bba7
aditi040306 <aditi0603verma@gmail.com>
2026-06-30 16:57:40 -0500

    Update controller comment for clarity

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -2,7 +2,7 @@ from flask import jsonify, session
 from ..models.user import find_user_by_username, create_user
 
 # Author : av42956 
-# Controller : to manage login and register user
+# Controller : to manage both login and register user
 
 def handle_register(request):
     data = request.get_json()
